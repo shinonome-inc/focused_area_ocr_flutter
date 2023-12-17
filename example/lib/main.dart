1b470e4bbcd0de2e5de2d714b8bc82b57745e27b
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2023-12-17 22:49:55 +0900

    fix import

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:focused_area_ocr_flutter/focused_area_ocr_view.dart';
+import 'package:focused_area_ocr_flutter/focused_area_ocr_flutter.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
